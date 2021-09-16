161fcbe82c5d258ce4ad924df9e1494848d0117c
jasmin3ashl3y <jasmine@ashleyit.com>
2021-09-16 15:53:16 -0400

    made button functional/clicked

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+//console.dir(window.document);""
+
+var buttonEl = document.querySelector("#save-task");
+
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+//tasksToDoEl.appendChild(taskItemEl);
+
+//var taskItemEl = document.createElement("li");
+//taskItemEl.textContent = "hello";
+
+//taskItemEl.className = "task-item";
+
+buttonEl.addEventListener("click", function() {
+    alert("button clicked");
+});
+  //var listItemEl = document.createElement("li");
+  //listItemEl.className = "task-item";
+  //listItemEl.textContent = "This is a new task.";
+  //tasksToDoEl.appendChild(listItemEl);
+//});
